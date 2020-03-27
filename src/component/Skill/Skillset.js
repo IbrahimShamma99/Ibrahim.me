4b52bd9024276d08606845a1ecb06d9ba039970f
ibrahimshamma99 <i.abushammah@gmail.com>
2020-03-27 14:39:42 +0300

    update

--- src/component/Skill/Skillset.js
+++ src/component/Skill/Skillset.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import "./Skillset.css"
+
+//NOTE LOGOS
 import NodeLogo from '../../img/node.svg';
 import ReactLogo from "../../img/react.svg";
 import BootStrapLogo from "../../img/bootstrap.svg";
+import MngoLogo from '../../img/mongodb-icon.svg';
+import LnxLogo from '../../img/linux-icon.svg';
+import DkrLogo from '../../img/docker-icon.svg';
+import PyLogo from '../../img/python-brands.svg';
+import ExLogo from '../../img/expressjs-icon.svg';
+import RdxLogo from '../../img/redux-icon.svg';
+import JsLogo from '../../img/js-square-brands.svg';
+import TfLogo from '../../img/tensorflow-icon.svg';
+import TsLogo from '../../img/typescriptlang-icon.svg';
+import RstLogo from '../../img/rust-lang-icon.svg';
+import CppLogo from '../../img/cpp.svg';
 
 const Skill = (props)=>(
     <div className="progress mb-2 ml-2">
@@ -27,19 +40,19 @@ const skillset = (props)=>(
                 <h1 className="home-style navbar-brand font-weight-bold">Stack</h1>
                 <Skill logo={NodeLogo} styling="progress-bar bg-danger" skill="NodeJS" progress="85" />
                 <Skill logo={ReactLogo} styling="progress-bar bg-danger" skill="ReactJS" progress="75"/>
-                <Skill styling="progress-bar bg-danger" skill="Express" progress="85"/>
-                <Skill styling="progress-bar bg-danger" skill="MongoDB" progress="80"/>
-                <Skill styling="progress-bar bg-danger" skill="Redux" progress="65"/>
+                <Skill logo={ExLogo} styling="progress-bar bg-danger" skill="Express" progress="85"/>
+                <Skill logo={MngoLogo} styling="progress-bar bg-danger" skill="MongoDB" progress="80"/>
+                <Skill logo={RdxLogo} styling="progress-bar bg-danger" skill="Redux" progress="65"/>
             </div>
 
             <div class="col" align="center">
                 <h1 className="home-style navbar-brand 
                 font-weight-bold">Languages</h1>
-                <Skill styling="progress-bar bg-warning" skill="Javascript" progress="80"/>
-                <Skill styling="progress-bar bg-warning" skill="Typescript" progress="70"/>
-                <Skill styling="progress-bar bg-warning" skill="Python" progress="75"/>
-                <Skill styling="progress-bar bg-warning" skill="C++" progress="60"/>
-                <Skill styling="progress-bar bg-warning" skill="Rust" progress="50"/>
+                <Skill logo={JsLogo} styling="progress-bar bg-warning" skill="Javascript" progress="80"/>
+                <Skill logo={TsLogo} styling="progress-bar bg-warning" skill="Typescript" progress="70"/>
+                <Skill logo={PyLogo} styling="progress-bar bg-warning" skill="Python" progress="75"/>
+                <Skill logo={CppLogo} styling="progress-bar bg-warning" skill="C++" progress="60"/>
+                <Skill logo={RstLogo} styling="progress-bar bg-warning" skill="Rust" progress="50"/>
             </div>
             <div class="col" align="center">
                 <h1 className="home-style navbar-brand 
@@ -63,9 +76,9 @@ const skillset = (props)=>(
     
             <div class="col">
                 <h1  className="home-style navbar-brand font-weight-bold">Other skills</h1>
-                <Skill styling="progress-bar bg-success" skill="Machine Learning" progress="65"/>
-                <Skill styling="progress-bar bg-success" skill="Docker" progress="60"/>
-                <Skill styling="progress-bar bg-success" skill="Linux" progress="70"/>
+                <Skill logo={TfLogo} styling="progress-bar bg-success" skill="Machine Learning" progress="65"/>
+                <Skill logo={DkrLogo} styling="progress-bar bg-success" skill="Docker" progress="60"/>
+                <Skill logo={LnxLogo} styling="progress-bar bg-success" skill="Linux" progress="70"/>
             </div>
         </div>
     </div>
